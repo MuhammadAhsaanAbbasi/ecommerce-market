65f53bf52bb9d80c89bcec82dc8d411d668e2c9b
Muhammad Ahsaan Abbasi <142156097+MuhammadAhsaanAbbasi@users.noreply.github.com>
2024-06-12 18:10:50 +0500

    Update route.ts

--- src/app/api/auth/sign-in/route.ts
+++ src/app/api/auth/sign-in/route.ts
@@ -1,37 +1,37 @@
-import { NextRequest, NextResponse } from "next/server";
-import { LoginSchema } from "../../../../../schemas";
-import { getUserByEmail } from "@/data/user";
-import { signIn } from "../../../../../auth";
-import { DEFAULT_LOGIN_REDIRECT } from "../../../../../routes";
-import { AuthError } from "next-auth";
+// import { NextRequest, NextResponse } from "next/server";
+// import { LoginSchema } from "../../../../../schemas";
+// import { getUserByEmail } from "@/data/user";
+// import { signIn } from "../../../../../auth";
+// import { DEFAULT_LOGIN_REDIRECT } from "../../../../../routes";
+// import { AuthError } from "next-auth";
 
-export async function POST(request: NextRequest) {
-    const res = await request.json()
-    const validatedFields = LoginSchema.safeParse(res)
-    if (!validatedFields.success) {
-        return { error: "Invalid Fields "! }
-    }
-    try {
-        const { email, password } = validatedFields.data
-        const existingUser = await getUserByEmail(email)
-        if (!existingUser || !existingUser.email || !existingUser.password) {
-            return { error: "Incorrect Email or Password!" }
-        }
-        await signIn("credentials", {
-            email,
-            password,
-            redirectTo: DEFAULT_LOGIN_REDIRECT
-        })
-    } catch (error) {
-        if (error instanceof AuthError) {
-            switch (error.type) {
-                case "CredentialsSignin":
-                    return { error: "Invalid credentials!" }
-                default:
-                    return { error: "Something went wrong!" }
-            }
-        }
+// export async function POST(request: NextRequest) {
+//     const res = await request.json()
+//     const validatedFields = LoginSchema.safeParse(res)
+//     if (!validatedFields.success) {
+//         return { error: "Invalid Fields "! }
+//     }
+//     try {
+//         const { email, password } = validatedFields.data
+//         const existingUser = await getUserByEmail(email)
+//         if (!existingUser || !existingUser.email || !existingUser.password) {
+//             return { error: "Incorrect Email or Password!" }
+//         }
+//         await signIn("credentials", {
+//             email,
+//             password,
+//             redirectTo: DEFAULT_LOGIN_REDIRECT
+//         })
+//     } catch (error) {
+//         if (error instanceof AuthError) {
+//             switch (error.type) {
+//                 case "CredentialsSignin":
+//                     return { error: "Invalid credentials!" }
+//                 default:
+//                     return { error: "Something went wrong!" }
+//             }
+//         }
 
-        throw error;
-    }
-}
+//         throw error;
+//     }
+// }
